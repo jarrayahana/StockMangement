d0adef694a5e6eaf96b4fa0cf8a2f618bb47822f
hanajarrraya <jarrayahana786@gmail.com>
2026-06-09 19:22:45 +0100

    address ,admin, delivery partner controllers and routes and middleware

--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,8 @@ import orderRouter from "./routes/orderRoutes.js";
 
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import addressRouter from "./routes/addressRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 const app = express();
 
 // Middleware
@@ -24,6 +26,8 @@ app.use('/api/products',productRouter)
 app.use('/api/upload',uploadRouter)
 app.use('/api/orders',orderRouter)
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/addresses',addressRouter)
+app.use('/api/admin',adminRouter)
 //Error handling
 
 app.use((error:any,req: Request, res: Response,next:NextFunction)=>{
